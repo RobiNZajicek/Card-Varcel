2f35ade1652e2c2c3114f1eddaef242cfac98a68
Robin Zajicek <zajicekrobin3@gmail.com>
2024-03-31 21:24:31 +0200

    vercel-card\

---
+++ src/components/CardSafe/CardSafe.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CardSafe = () => {
+
+    
+
+  return <section>
+        <div>
+
+        </div>
+  </section>
+}
+
+export default CardSafe
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import logo from '../../assets/NavbarLogo.jpg'
+import {projectFirestore} from '../../firebase/config'
+import './Card.css'
+const Card = ({ id, celed, rod, druh, puvod, popsal}) => {
+  return <div key={id} className='the-card'>
+        {/* <div>
+              {allAnswers.map((oneCard) => (
+                    <Card key={oneCard.id} {...oneCard} />
+              ))}
+        </div> */}
+        
+        
+      <div className='the-card-left'>
+            <p className='under'>{celed}</p>
+            <p className='under'>{rod}</p>
+            <p className='under'>{druh}</p>
+            <p className='under' >{puvod}</p>
+            <p className='under'>{popsal}</p>
+        </div>
+            <div className='the-card-right'>
+                   <img className='the-card-logo' src={logo} alt="" /> 
+                   <span id='text-logo' className='mt-12'>emberizashells.cz</span>
+            </div>
+            </div>
+    
+
+}
+
+export default Card
